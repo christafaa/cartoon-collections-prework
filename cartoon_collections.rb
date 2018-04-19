d4f05a9c3fa84b7f04d3e223433d8046e639bcdf
Chris Alberti <christopherjalberti@gmail.com>
2018-04-19 02:26:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(array)
   array.map {|ele| ele.capitalize << "!"}
 end
 
-def long_planeteer_calls
-  
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese
